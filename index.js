92f55ddb24a61cdeffbee6a39d4226cfd3779ba3
hiro88hyo <hiro88hyo@gmail.com>
2016-05-03 10:54:14 +0000

    fix index and declaration

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ var PushBullet = require('pushbullet');
 var moment = require('moment-timezone');
 
 exports.handler = function(event, context) {
-    console.log(JSON.stringify({event: event, context: context}, null, 2));
-    method = event['context']['http-method'];
+  console.log(JSON.stringify({event: event, context: context}, null, 2));
+  var method = event['context']['http-method'];
 
-    switch(method){
-        case "GET":
-            console.log(event);
-            context.succeed({event: event, method: method});
-            break;
-        case "POST":
-            var json = JSON.parse(xml2json.toJson(event['body']));
-            var entry = json['feed']['entry'][0];
-            var updated = new Date(Date.parse(entry['updated']));
-            var content = moment(updated).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm:ss') + 
-                          '\n' + entry['content']['$t'];
+  switch(method){
+    case "GET":
+      console.log(event);
+      context.succeed({event: event, method: method});
+      break;
+    case "POST":
+      var json = JSON.parse(xml2json.toJson(event['body']));
+      var entry = json['feed']['entry'][0];
+      var updated = new Date(Date.parse(entry['updated']));
+      var content = moment(updated).tz('Asia/Tokyo')
+                    .format('YYYY/MM/DD HH:mm:ss') + 
+                    '\n' +
+                    entry['content']['$t'];
 
-            lambda.getFunctionConfiguration({FunctionName:context.functionName},function(err,result){
-              pushbullet_access_token = JSON.parse(result.Description)['pushbullet_key'];
-              var pusher = new PushBullet(pushbullet_access_token);
-              pusher.note("", entry['title'], content, function(error, response){
-                context.succeed({event: event, method: method});
-              });
-            });
-            break;
-        default:
-            context.succeed({});
-    }
+      lambda.getFunctionConfiguration({FunctionName:context.functionName},function(err,result){
+        var pushbullet_access_token = JSON.parse(result.Description)['pushbullet_key'];
+        var pusher = new PushBullet(pushbullet_access_token);
+        pusher.note("", entry['title'], content, function(error, response){
+          context.succeed({event: event, method: method});
+        });
+      });
+      break;
+    default:
+      context.succeed({});
+  }
 };
